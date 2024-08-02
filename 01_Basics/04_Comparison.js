e4211c190172fa34769de43d31eaebe0b14b8925
Harish Patil <harishap000@gmail.com>
2024-08-02 20:11:22 +0530

    Adding New File

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,8 @@
+/* ***************** basic comparison******************* */ 
+console.log(3 > 2);
+console.log(3 < 2);
+console.log(3 <= 2);
+console.log(3 >= 2);
+console.log( "2" == 2);
+console.log( "2" === 2);
+console.log( 2 === 2);
