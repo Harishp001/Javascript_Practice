d62be34d21e7d0842a75148534117fa3c083a216
Harish Patil <harishap000@gmail.com>
2024-09-04 15:06:21 +0530

    String Example

---
+++ 01_Basics/05_String.js
@@ -0,0 +1,19 @@
+const name = "Harish"
+const repoCount = 7
+
+//console.log(name + repoCount + "correct");
+
+console.log(`My name is ${name} and my repoCount is ${repoCount}`);
+
+const userName = new String('harish-patil')
+
+console.log(userName[0]);
+console.log(userName.length);
+console.log(userName.toUpperCase());
+console.log(userName.charAt(4));
+console.log(userName.indexOf('p'));
+
+
+
+
+
