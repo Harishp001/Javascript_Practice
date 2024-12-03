7ff25273dc7671c7e33ff90b90d8e8fc6ecd3840
Harish Patil <harishap000@gmail.com>
2024-12-03 11:51:43 +0530

    new

--- 02_Basics/02_Arrays.js
+++ 02_Basics/02_Arrays.js
@@ -4,10 +4,10 @@ const n3 = [11,12,13,14]
 
 // n1.push(n2) //push add array into array
 
-const newArr = n1.concat(n2,n3)            //concat
+const newArr = n1.concat(n2,n3)            //concat array and return new array
 // console.log(newArr);
 
-const newArr1 = [...n1, ...n2, ...n3]      //spread
+const newArr1 = [...n1, ...n2, ...n3]      //spread same as concat but we can give multiple array S
 // console.log(newArr1);
 
 const complexArray = [1,2,3,[4,5,6],7,8,[9,[10]]]
